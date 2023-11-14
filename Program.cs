5ef2f91dd128dcda61eecc5cdac829ba37c07a6b
Angelicalindström <nutrition.angelica@gmail.com>
2023-11-14 10:20:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Branch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // skapa referens av en employee och lägg in lite data
+            // skapa en metod som startar programmet
+        }
+    }
+}
